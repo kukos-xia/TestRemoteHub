f10410452e68d279dbdcc2d6c349c5c42625e2cf
xiajunkang <2198043221@qq.com>
2026-05-31 20:04:34 +0800

    branch of PictureService

--- PictureService.java
+++ PictureService.java
@@ -94,6 +94,7 @@ public interface PictureService extends IService<Picture> {
     /**
      * 删除旧图片
      * @param oldpicture
+	123456789
      */
     public void clearPictureFile(Picture oldpicture);
 }
